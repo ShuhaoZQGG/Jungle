30147b0d0cd045424ec586a6b4439e0c5dee97ce
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-12 18:22:24 -0500

    set up the table for sales and status

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,4 +1,10 @@
 class Admin::SalesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: []
+  
   def index
+    @sales = Sale.all
+  end
+
+  def new
   end
 end
