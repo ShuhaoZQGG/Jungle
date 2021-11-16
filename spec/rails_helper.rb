661a09bd8fcc2e6c963937d6c061a4c5baa9784b
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-16 15:56:16 -0500

    write feature rspec test using capybara, and phantomJS

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist" # Add this line to require poltergeist
+
 
 ENV['RAILS_ENV'] ||= 'test'
 
@@ -14,7 +12,9 @@ Capybara.javascript_driver = :poltergeist
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist' # Add this line to require poltergeist
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -45,7 +45,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
