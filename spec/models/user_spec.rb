323f32e22c3be8fedf44ac83c294650ce4e4e938
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-15 22:02:51 -0500

    test if name is missing in creating a new user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe User, type: :model do
     end
 
     it "successfully creating a new user" do
-      @new_user = User.new(:name => "rspec", :email => "rspec@gmail.com", :password_digest => "rspec")
+      @new_user = User.new(name: "rspec_test", email: "rspec_test@gmail.com", password: "rspec_test", password_confirmation: "rspec_test")
       @new_user.save!
       expect(@new_user).to be_persisted
     end
 
     it "return error message if name is missing" do
-      @new_user = User.new(:email => "rspec@gmail.com", :password_digest => "rspec")
-      @new_user.save!
+      @new_user = User.new(email: "rspec_test@gmail.com", password: "rspec_test", password_confirmation: "rspec_test")
+      expect(@new_user).to_not be_valid
       expect(@new_user.errors.messages[:name]).to include('can\'t be blank')
     end
   end
