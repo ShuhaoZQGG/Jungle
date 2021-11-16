1b68bd560fa50cc906b2dbb51781a42249e5cb38
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-15 21:55:24 -0500

    change user_auth to display error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation for user" do
+    before do
+      @user = User.new
+    end
+
+    it "successfully creating a new user" do
+      @new_user = User.new(:name => "rspec", :email => "rspec@gmail.com", :password_digest => "rspec")
+      @new_user.save!
+      expect(@new_user).to be_persisted
+    end
+
+    it "return error message if name is missing" do
+      @new_user = User.new(:email => "rspec@gmail.com", :password_digest => "rspec")
+      @new_user.save!
+      expect(@new_user.errors.messages[:name]).to include('can\'t be blank')
+    end
+  end
 end
